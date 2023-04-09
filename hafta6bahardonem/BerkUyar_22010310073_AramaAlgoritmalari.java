efce122911e27f2a615684f0203f0b2350ad6cd2
berkuyar <berkuyar5534@gmail.com>
2023-04-09 21:34:03 +0300

    testdenemesi

---
+++ hafta6bahardonem/BerkUyar_22010310073_AramaAlgoritmalari.java
@@ -0,0 +1,108 @@
+package hafta6bahardonem;
+
+public class BerkUyar_22010310073_AramaAlgoritmalari {
+
+	public static void main(String[] args) {
+		
+		int [] numbers = { 63, 18, 25, 30, 11, 20, 90, 5 };
+		int [] numbers2 = { 5, 11, 18, 20, 25, 30, 63, 90 };
+		
+		int index = indexOf(numbers, 5);
+		if(index != -1) {
+			System.out.println("Aranan eleman bulundu. " + (index + 1) + " sirada");
+		}	
+		else {
+			System.out.println("Aranan eleman bulunamadi.");
+		
+		}
+		 index = indexOf2(numbers2, 90);
+		if(index != -1) {
+			System.out.println("Aranan eleman bulundu. " + (index + 1) + " sirada");
+		}	
+		else {
+			System.out.println("Aranan eleman bulunamadi.");
+		
+		}
+		 index = binarySearch(numbers2, 20);
+		if(index != -1) {
+			System.out.println("Aranan eleman bulundu. " + (index + 1) + " sirada");
+		}	
+		else {
+			System.out.println("Aranan eleman bulunamadi.");
+		}
+		int [] numbers3 = {1, 3, 12, 14, 23, 34, 55, 65, 75, 78 };
+		index = binarySearchR(numbers3, 80 , 0, numbers3.length-1);
+			if(index != -1) {
+				System.out.println("Aranan eleman bulundu. " + index  + " sirada");
+			}	
+			else {
+				System.out.println("Aranan eleman bulunamadi.");
+			
+			}
+		}
+	
+		public static int binarySearchR(int [] numbers, int target, int min, int max) {
+			if(min > max) {
+				return -1;
+			}
+			else {
+			
+			int mid = (min + max) / 2;
+			if(numbers[mid] == target) {
+				return mid;
+			}
+			else if (numbers [mid] < target) {
+				return binarySearchR(numbers, target, mid+1, max);
+				
+			}
+			else {
+				return binarySearchR(numbers, target, min, mid-1);
+			}
+		}
+		
+	}
+	
+	public static int binarySearch(int [] numbers, int target) {
+		int min = 0;
+		int max = numbers.length-1;
+		
+		while (min <= max) {
+			int mid = (max + min ) / 2;
+			if(numbers [mid] == target) {
+				return mid;
+			}
+			
+			else if (numbers [mid] < target) {
+				min = mid + 1;
+			}
+			else { 
+				max = mid - 1;
+			}
+			
+		}
+		
+		return -1;
+	}
+
+	public static int indexOf(int [] numbers, int target) {
+		for (int index = 0; index < numbers.length; index++) {
+			if (numbers [index] == target) {
+				return index;
+			}
+		}
+		return -1;
+	}
+	public static int indexOf2(int [] numbers, int target) {
+		int index = 0;
+		while (index < numbers.length && numbers [index] <= target) {
+			if (numbers [index] == target) {
+				return index;
+			}
+			else {
+				index ++;
+			}
+		}
+		
+		return -1;
+	}
+}
